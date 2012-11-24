e56477f2a9ba8edf15d2c38874f6c8f22b7b1682
Dominik Kundel <dominik.kundel@me.com>
2012-11-24 02:55:39 +0100

    implement callback main fetch

--- scripts/main_page/main_page.js
+++ scripts/main_page/main_page.js
@@ -24,8 +24,8 @@
         _init:function () {
             this.container = $('div.content_wrapper');
             this._fetch_content();
-            this._build_content_page();
-            this._display_page();
+            //this._build_content_page();
+            //this._display_page();
         },
         /**
          *
@@ -36,7 +36,18 @@
                 function:"get_ceva"
             }
             $.post("scripts/load_function.psp", data, function (data) {
-                console.log(data);
+                content = '<div class="row-fluid"><ul class="thumbnails">';
+                for(int i=0; i < data.length; ++i){
+                        content += '<li class="span3 product"><div class="thumbnail">'+
+                        '<img src="http://placehold.it/300x200" alt=""><div class="caption">';
+                        content += '<h3>' + data[i][1] + '</h3>';
+                        content += '<p>' + data[i][2] + '</p>';
+                        content += '<p><a href="#' + data[i][0] + '" class="btn btn-jmarket-orange"><i class="icon-share icon-white"></i> Details</a></p>';
+                        content += '<h3 class="price-tag">' + data[i][3] + '</h3>';
+                        content += '<div class="clearfix"></div></div></div></li>';
+                }
+                this.page_html = content;
+                this._display_page();
             });
         },
         /**
