2936f2c6639f6db6f3bd57c5c823cc50374d402d
gmerticariu <merticariug@gmail.com>
2012-11-26 14:40:35 +0100

    nothing new

--- scripts/main_page/main_page.js
+++ scripts/main_page/main_page.js
@@ -7,25 +7,10 @@
     MainPage.prototype = {
         /**
          *
-         */
-        data_content:null,
-        /**
-         *
-         */
-        page_html:null,
-        /**
-         *
-         */
-        container:null,
-        /**
-         *
          * @private
          */
         _init:function () {
-            this.container = $('div.content_wrapper');
             this._fetch_content();
-            //this._build_content_page();
-            //this._display_page();
         },
         /**
          *
@@ -38,32 +23,18 @@
             $.post("scripts/load_function.psp", data, function (data) {
                 data = JSON.parse(data);
                 content = '<div class="row-fluid"><ul class="thumbnails">';
-                for(var i=0; i < data.length; ++i){
-                        content += '<li class="span3 product"><div class="thumbnail">'+
+                for (var i = 0; i < data.length; ++i) {
+                    content += '<li class="span3 product"><div class="thumbnail">' +
                         '<img src="http://placehold.it/300x200" alt=""><div class="caption">';
-                        content += '<h3>' + data[i][1] + '</h3>';
-                        content += '<p>' + data[i][2] + '</p>';
-                        content += '<p class="caption_bottom"><a href="#' + data[i][0] + '" class="btn btn-jmarket-orange"><i class="icon-share icon-white"></i> Details</a></p>';
-                        content += '<h3 class="price-tag">' + data[i][3] + '</h3>';
-                        content += '<div class="clearfix"></div></div></div></li>';
+                    content += '<h3>' + data[i][1] + '</h3>';
+                    content += '<p>' + data[i][2] + '</p>';
+                    content += '<p class="caption_bottom"><a href="#' + data[i][0] + '" class="btn btn-jmarket-orange"><i class="icon-share icon-white"></i> Details</a></p>';
+                    content += '<h3 class="price-tag">' + data[i][3] + '</h3>';
+                    content += '<div class="clearfix"></div></div></div></li>';
                 }
-                this.page_html = content;
+                content+="</ul></div>"
                 $('div.content_wrapper').html(content);
             });
-        },
-        /**
-         *
-         * @param item
-         * @private
-         */
-        _build_product_container:function (item) {
-
-        },
-        _build_content_page:function () {
-
-        },
-        _display_page:function () {
-            this.container.html(this.page_html);
         }
 
     }
---
+++ values.sql
@@ -0,0 +1,168 @@
+USE jMarket;
+
+INSERT INTO `categories` (`name`,`type`) VALUES
+                         ('pants','clothing'),
+                         ('shirts','clothing'),
+                         ('jackets','clothing'),
+                         ('shoes','clothing'),
+                         ('others','clothing'),
+                         ('mobile phones','technology'),
+                         ('laptops','technology'),
+                         ('monitors','technology'),
+                         ('tablets','technology'),
+                         ('printers','technology'),
+                         ('others','technology'),
+                         ('sofas','furniture'),
+                         ('chairs','furniture'),
+                         ('wardrobes','furniture'),
+                         ('tables','furniture'),
+                         ('others','furniture'),
+                         ('others','others');
+
+INSERT INTO `product` (`name`,`description`,`picture_path`) VALUES
+                      ('Jeans size 32','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture1.png'),
+                      ('Red T-shirt size L','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture2.png'),
+                      ('Black jacket size XL','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture3.png'),
+                      ('Sport shoes 41','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture4.png'),
+                      ('Iphone 5','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture5.png'),
+                      ('Ipod touch','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture6.png'),
+                      ('Google nexus 4','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture7.png'),
+                      ('Samsung monitor 22 inch','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture8.png'),
+                      ('Lan cable 20m','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture9.png'),
+                      ('Ikea small table','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture10.png'),
+                      ('small shelf','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture11.png'),
+                      ('Black chair','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture12.png'),
+                      ('Leather sofa 3 seats','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture13.png'),
+                      ('Small fridge','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture14.png'),
+                      ('Water boiler','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture15.png'),
+                      ('Bike men','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture16.png'),
+                      ('Red carpet','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture17.png'),
+                      ('Cloth rack','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture18.png'),
+                      ('Desk lamp','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture19.png'),
+                      ('Big poster','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','/img/products/picture20.png');
+
+
+
+INSERT INTO `users` (`email`,`password`,`address`,`phone`,`type`,`createtime`) VALUES
+                    ('g.merticariu@jacobs-university.de',MD5('password123'),'College Ring 7, XA - 125','5646','admin',NOW()),
+                    ('d.kundel@jacobs-university.de',MD5('password123'),'College Ring 7, XA - 126','5647','admin',NOW()),
+                    ('m.ercu@jacobs-university.de',MD5('password123'),'College Ring 7, XA - 127','5648','regular',NOW()),
+                    ('m.baltac@jacobs-university.de',MD5('password123'),'College Ring 7, XA - 128','5649','regular',NOW());
+
+
+INSERT INTO `selling_products` (`user_id`,`product_id`,`price`) VALUES
+                               (10,25,10), (11,26,5),
+                               (12,27,25),(13,28,10),
+                               (10,29,400),(11,30,150),
+                               (12,31,250),(13,32,100),
+                               (10,33,5),(11,34,15),
+                               (12,35,18),(13,36,10),
+                               (10,37,40),(11,38,40),
+                               (12,39,12),(13,40,80),
+                               (10,41,25),(11,42,10),
+                               (12,43,5),(13,44,8);
+
+
+
+
+
