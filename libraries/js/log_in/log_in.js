0e14be357db8f954a57bf256d4bded34523dccf6
gmerticariu <merticariug@gmail.com>
2012-11-28 20:57:18 +0100

    a

--- libraries/js/log_in/log_in.js
+++ libraries/js/log_in/log_in.js
@@ -7,17 +7,34 @@ var LogIn = {
 
     log_in:function (form) {
         LogIn.requestData.user = form.find("input#username").val();
-        LogIn.requestData.password =form.find("input#password").val();
+        LogIn.requestData.password = form.find("input#password").val();
         LogIn._send_request();
     },
 
     _send_request:function () {
         $.post(window.jMarket.requestUrl, LogIn.requestData, function (data) {
-        data = JSON.parse(data)
-        if (data.error){
-            window.jMarket.Modules.DisplayMessage.print(data.error,'error');
-        }
+            data = JSON.parse(data)
+            if (data.error) {
+                window.jMarket.Modules.DisplayMessage.print(data.error, 'error');
+                return;
+            }
+            var date = new Date();
+            date.setTime(date.getTime() + (30 * 60 * 1000));
+            $.cookie('user_id', data.user_id, { expires:date, path:'/' });
+            if ($.cookie('user_id')) {
+                window.jMarket.Modules.DisplayMessage.print("Login successful", "success");
+                window.jMarket.Modules.LogIn._logout_button();
+            } else {
+                window.jMarket.Modules.DisplayMessage.print("Login failed. Please try again later.", 'error');
+            }
+
+
         });
+    },
+
+    _logout_button:function () {
+        $('li.login').hide();
+        $('li.logout').show();
     }
 
 };
