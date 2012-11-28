3d3de2f614ae5237a807b6a1f21caf3895c84762
gmerticariu <merticariug@gmail.com>
2012-11-28 22:51:42 +0100

    remove display message after time interval

--- libraries/js/modules/display_message.js
+++ libraries/js/modules/display_message.js
@@ -1,17 +1,20 @@
 var DisplayMessage = {
-    type: new Array(
+    type:new Array(
         'info',
         'success',
         'error',
         'warning'
     ),
-    print:function(message, type){
-        if (DisplayMessage.type.indexOf(type) == -1){
+    print:function (message, type) {
+        if (DisplayMessage.type.indexOf(type) == -1) {
             return;
         }
-
+        $('.user-message').remove();
         var close = $('<a style="cursor: pointer; float: right; margin-right: 10px;" onclick="javascript:$(this).parent().fadeOut(function(){$(this).remove()});"><i class="icon-remove-circle"></i></a>');
-        $('body').append( $('<div style="position: absolute; top: 25px; left: 25%; word-wrap: break-word;" class="alert alert-'+type+' span7 box-shadow">'+message+'</div>').prepend(close).fadeIn() );
+        $('body').append($('<div style="position: absolute; top: 25px; left: 25%; word-wrap: break-word;" class="alert alert-' + type + ' span7 box-shadow user-message">' + message + '</div>').prepend(close).fadeIn());
+        setTimeout(function () {
+            $('.user-message').remove();
+        }, 8000);
     }
 };
 
