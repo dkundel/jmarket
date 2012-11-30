18de543e8f3dd37843e122df20b0020145114760
Dominik Kundel <dominik.kundel@me.com>
2012-11-30 18:36:02 +0100

    chaneg product.js

--- libraries/js/modules/product.js
+++ libraries/js/modules/product.js
@@ -6,13 +6,12 @@ var Product = {
 		html += '<img src="' + data[3] + '" alt="Product No. ' + data[0] + '">';
 		html += '</div></div><div class="span6">';
 		html += '<p>' + data[2] + '</p>';
-		html += '<p><h3 class="price-tag">' + data[5] + '</h3>&nbsp;';
-		html += '<a class="btn btn-jmarket-orange" href="mailto:' + data[8] + '">Contact Seller</a></p></div>';
+		html += '<p><h3 class="price-tag">' + data[4] + '</h3>&nbsp;';
+		html += '<a class="btn btn-jmarket-orange" href="mailto:' + data[7] + '">Contact Seller</a></p></div>';
 		html += '</div><div class="row"><div class="span10"><p></p>';
-		html += '<p><i class="icon-user"></i> by <a href="javascript:window.jMarket.Modules.User.getProfilePage(' + user[7] +');">';
-		html += (data[8].split("@jacobs-university.de"))[0] + "</a> | ";
-		html += '<i class="icon-calendar"></i> ' + (new Date(data[4])).toLocaleDateString() + " | ";
-		html += '<i class="icon-tags"></i> Category : <span class="label label-warning">' + data[9] + '</span>';
+		html += '<p><i class="icon-user"></i> by <a href="javascript:window.jMarket.Modules.User.getProfilePage(' + user[6] +');">';
+		html += (data[7].split("@jacobs-university.de"))[0] + "</a> | ";
+		html += '<i class="icon-tags"></i> Category : <span class="label label-warning">' + data[8] + '</span>';
 		html += '</p></div>';
 		$('div.content_wrapper').html(html);
 	},
